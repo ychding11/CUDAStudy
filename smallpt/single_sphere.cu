e3f65c21bbcd5c3603d98e490ab2d75d00503938
ychding11 <ych_ding@163.com>
2018-08-18 00:40:25 +0800

    add random number generator.

--- smallpt/single_sphere.cu
+++ smallpt/single_sphere.cu
@@ -3,7 +3,8 @@
 #include <iostream>
 #include <chrono>  // for high_resolution_clock
 #include <cuda_runtime.h>
-//#include <vector_types.h>
+#include <curand.h>
+#include <curand_kernel.h>
 #include "device_launch_parameters.h"
 #include "helper_string.h"
 #include "ray.h"
@@ -60,8 +61,17 @@ __device__ vec3 color(const ray& r)
 }
 
 
+__global__ void setup_random_kernel(curandState *states, int nx, int ny)
+{
+    unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
+    unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
+    unsigned int i = (ny - y - 1) * nx + x; // index of current pixel (calculated using thread index) 
+
+    curand_init(1234, i, 0, &states[i]);
+}
+
 // __global__ : executed on the device (GPU) and callable only from host (CPU) 
-__global__ void render_kernel(float* output, int nx, int ny, int ns)
+__global__ void render_kernel(curandState *states, float* output, int nx, int ny, int ns)
 {
     unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
     unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
@@ -72,15 +82,19 @@ __global__ void render_kernel(float* output, int nx, int ny, int ns)
     vec3 vertical(0.f, 2.f, 0.f);
     vec3 origin(0.f, 0.f, 0.f);
 
+    curandState localState = states[i];
     vec3 *pic = (vec3*)output;
     vec3 col(0.f, 0.f, 0.f);
     for (int s = 0; s < ns; s++)
     {
-        float u = float(x ) / float(nx);
-        float v = float(y ) / float(ny);
+        float dx = curand_uniform(&localState);
+        float dy = curand_uniform(&localState);
+        float u = float(x + dx) / float(nx);
+        float v = float(y + dy) / float(ny);
         ray r(origin, low_left_corner + u * horizonal + v * vertical);
         col += color(r);
     }
+    states[i] = localState;
     col /= float(ns);
     pic[i] = col;
 }
@@ -96,6 +110,8 @@ int TestSmallPTOnGPU(int width, int height, int samps)
     float* output_h = new float[width * height * 3]; // pointer to memory for image on the host (system RAM)
     float* output_d;    // pointer to memory for image on the device (GPU VRAM)
 
+    std::chrono::duration<double> elapsed;
+
     CUDA_CALL_CHECK( cudaSetDevice(0) );
 
     CUDA_CALL_CHECK( cudaMalloc(&output_d, width * height * sizeof(float) * 3) );
@@ -103,20 +119,29 @@ int TestSmallPTOnGPU(int width, int height, int samps)
     dim3 block(8, 8, 1);   
     dim3 grid(width / block.x, height / block.y, 1);
 
+    // Record start time                          
+    auto startRand = std::chrono::high_resolution_clock::now();
+    curandState *devStates;
+    CUDA_CALL_CHECK(cudaMalloc((void **)&devStates, width * height * sizeof(curandState)));
+    setup_random_kernel <<< grid, block >>>(devStates, width, height); 
+    CUDA_CALL_CHECK(cudaGetLastError());
+    CUDA_CALL_CHECK(cudaDeviceSynchronize());
+    auto finishRand = std::chrono::high_resolution_clock::now();
+    elapsed = finishRand - startRand;
+    printf("Random State Done! Time=%lf seconds\n", elapsed.count());
+
     printf("\nStart rendering... %d, %d, %d\n", width, height, samps);
  
     // Record start time                          
     auto start = std::chrono::high_resolution_clock::now();
 
-    render_kernel <<< grid, block >>>(output_d, width, height, samps);  
-
+    render_kernel <<< grid, block >>>(devStates, output_d, width, height, samps);  
     CUDA_CALL_CHECK(cudaGetLastError());
     CUDA_CALL_CHECK(cudaDeviceSynchronize());
 
     // Record end time
     auto finish = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = finish - start;
-
+    elapsed = finish - start;
     printf("Render Done! Time=%lf seconds\n", elapsed.count());
 
     CUDA_CALL_CHECK(cudaMemcpy(output_h, output_d, width * height * sizeof(float3), cudaMemcpyDeviceToHost));
@@ -131,7 +156,7 @@ int TestSmallPTOnGPU(int width, int height, int samps)
 
 int main(int argc, char *argv[])
 {
-    int width = 512, height = 512, samps = 1;
+    int width = 512, height = 512, samps = 1024;
     
     if (argc > 1)
     {
