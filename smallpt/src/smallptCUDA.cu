cdc47e33f9d7b70243f3e7ef148b1b4a1430434b
Yaochuang <ych_ding@163.com>
2021-06-06 17:29:18 +0800

    support simple AA.

--- smallpt/src/smallptCUDA.cu
+++ smallpt/src/smallptCUDA.cu
@@ -206,15 +206,44 @@ __global__ void render_kernel(float3 *output, int width, int height, int samps, 
     //< AA : samples per pixel
     //< It is NOT progressive mode
     float invSamps = (1. / samps);
-    for (int s = 0; s < samps; s++)
+    for (int s = 0; s < (samps >> 2); s++)
 	{  
-		//< ray sampling from camera 
-        float u = (2 * (x + 0.5f)) / float(width) - 1.0f ;
-        float v = (2 * (y + 0.5f)) / float(height) - 1.0f ;
-        float3 d = image_u * u + image_v * v + dir;
+        {
+		    //< ray sampling from camera 
+            float u = (2 * (x + 0.25f)) / float(width)  - 1.0f ;
+            float v = (2 * (y + 0.25f)) / float(height) - 1.0f ;
+            float3 d = image_u * u + image_v * v + dir;
 
-        // collect radiance 
-        r = r + radiance(Ray(cam.orig, normalize(d)), &s1, &s2) * invSamps;
+            // collect radiance 
+            r = r + radiance(Ray(cam.orig, normalize(d)), &s1, &s2) * invSamps;
+        }
+        {
+		    //< ray sampling from camera 
+            float u = (2 * (x + 0.75f)) / float(width)  - 1.0f ;
+            float v = (2 * (y + 0.25f)) / float(height) - 1.0f ;
+            float3 d = image_u * u + image_v * v + dir;
+
+            // collect radiance 
+            r = r + radiance(Ray(cam.orig, normalize(d)), &s1, &s2) * invSamps;
+        }
+        {
+		    //< ray sampling from camera 
+            float u = (2 * (x + 0.25f)) / float(width)  - 1.0f ;
+            float v = (2 * (y + 0.75f)) / float(height) - 1.0f ;
+            float3 d = image_u * u + image_v * v + dir;
+
+            // collect radiance 
+            r = r + radiance(Ray(cam.orig, normalize(d)), &s1, &s2) * invSamps;
+        }
+        {
+		    //< ray sampling from camera 
+            float u = (2 * (x + 0.75f)) / float(width)  - 1.0f ;
+            float v = (2 * (y + 0.75f)) / float(height) - 1.0f ;
+            float3 d = image_u * u + image_v * v + dir;
+
+            // collect radiance 
+            r = r + radiance(Ray(cam.orig, normalize(d)), &s1, &s2) * invSamps;
+        }
     }
 
     //< write raw rgb value to buffer on the GPU 
@@ -244,7 +273,7 @@ int TestSmallPTOnGPU(int width, int height, int samps, float vfov = 45.f)
     dim3 block(8, 8, 1);   
     dim3 grid(width / block.x, height / block.y, 1);
 
-    printf("\nStart rendering... %d, %d, %d\n", width, height, samps);
+    printf("\nStart rendering... %d, %d, %d, %f\n", width, height, samps, vfov);
  
     // Record start time                          
     auto start = std::chrono::high_resolution_clock::now();
