b49356185b0905964249ed0592b846dc4a687fb7
Yaochuang <ych_ding@163.com>
2021-07-08 13:32:13 +0800

    add utility class PerformanceProbe, still have bugs.

--- sphere/src/utility.h
+++ sphere/src/utility.h
@@ -2,13 +2,43 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 #include <chrono>  // for high_resolution_clock
 
 //#include <cuda_runtime.h>
 //#include <curand.h>
 //#include <curand_kernel.h>
 
+/////////////////////////////////////////////////////////////////
+// 
+/////////////////////////////////////////////////////////////////
 
+class PerformanceProbe
+{
+private: 
+    std::string m_name;
+    std::chrono::steady_clock::time_point m_start_time;
+    std::chrono::steady_clock::time_point m_end_time;
+public:
+    PerformanceProbe() = delete;
+    PerformanceProbe(const std::string name)
+        : m_name(name)
+    {
+         m_start_time = std::chrono::high_resolution_clock::now();
+    }
+
+    ~PerformanceProbe()
+    {
+         m_end_time = std::chrono::high_resolution_clock::now();
+         auto elapsed = m_end_time - m_start_time;
+         std::cout << "[Perfomance Probe] "
+             << m_name << " time = " << elapsed.count() << " second. "
+             << std::endl;
+    }
+};
+
+//< no space in X
+#define PERFORMANCE_PROBE(X) PerformanceProbe X{#X}
 
 inline float clamp(float x)
 {
@@ -29,12 +59,16 @@ inline bool file_exists(const std::string name)
 
 inline void SaveToPPM(float* output, int w, int h)
 {
-    // Write image to PPM file, a very simple image file format
-    FILE *f = fopen("smallptcuda.ppm", "w");          
-    fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
-    for (int i = 0; i < w * h * 3; i += 3)  // loop over pixels, write RGB values
-    fprintf(f, "%d %d %d ", toInt(output[i]), toInt(output[i + 1]), toInt(output[i + 2]));
-    fclose(f);
+    {
+        PERFORMANCE_PROBE(save_ppm);
+
+        // Write image to PPM file, a very simple image file format
+        FILE *f = fopen("smallptcuda.ppm", "w");          
+        fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
+        for (int i = 0; i < w * h * 3; i += 3)  // loop over pixels, write RGB values
+        fprintf(f, "%d %d %d ", toInt(output[i]), toInt(output[i + 1]), toInt(output[i + 2]));
+        fclose(f);
+    }
 
     system("ffplay smallptcuda.ppm");
 }
